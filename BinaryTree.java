09f3aef0e0b67c6c6572efd20dfb05b549d63d7d
syrilster <syrilster@gmail.com>
2016-07-06 10:35:09 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,10 @@
+
+package DataStructures;
+
+/**
+ * Created by syrils on 5/2/16.
+ */
+public class BinaryTree {
     Node root;
 
     static class Node {
@@ -8,6 +15,15 @@
             data = item;
             left = right = null;
         }
+    }
+
+    public static boolean isBST(Node root, int min, int max) {
+        if (root == null) return true;
+        if (root.data <= min || root.data > max) {
+            return false;
+        }
+        return isBST(root.left, min, root.data) && isBST(root.right, root.data, max);
+
     }
 
     public Node findLCA(int n1, int n2) {
@@ -40,7 +56,7 @@
 
     public static void main(String args[]) {
         BinaryTree tree = new BinaryTree();
-        /*tree.root = new Node(1);
+        tree.root = new Node(1);
         tree.root.left = new Node(2);
         tree.root.right = new Node(3);
         tree.root.left.left = new Node(4);
@@ -56,19 +72,6 @@
         System.out.println("LCA(2, 4) = " +
                 tree.findLCA(2, 4).data);*/
 
-        tree.root = new Node(10);
-        tree.root.left = new Node(10);
-        tree.root.right = new Node(19);
-        tree.root.left.left = new Node(-5);
-        tree.root.right.left = new Node(17);
-        tree.root.right.right = new Node(21);
-        System.out.println(isBST(tree.root, Integer.MIN_VALUE, Integer.MAX_VALUE));
+        System.out.print(isBST(tree.root, Integer.MIN_VALUE, Integer.MAX_VALUE));
     }
-
-    public static boolean isBST(Node root, int min, int max) {
-        if (root == null) return true;
-        if (root.data <= min || root.data > max) {
-            return false;
-        }
-        return isBST(root.left, min, root.data) && isBST(root.right, root.data, max);
-    }
+}
